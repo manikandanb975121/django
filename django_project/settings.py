14ae9f973b7d8fe9cb0286d309da28e39d5822e7
Manikandan <manikandab975121@gmail.com>
2019-10-07 10:58:31 +0530

    Added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,5 @@ EMAIL_HOST_PASSWORD = '06071999'
 
 #print(EMAIL_HOST_USER)
 #SECRET_KEY="3794052875c6d91dbccdb6ae90803b982d4f101d16d15780"
+
+django_heroku.settings(locals())
